45da25981c30734eacaf26af6eed4a27a4dd2d86
Lupino <lmjubuntu@gmail.com>
2019-08-28 10:00:08 +0800

    update payload length 128

--- sketch_aug23b.ino
+++ sketch_aug23b.ino
@@ -7,9 +7,10 @@ unsigned long sendTimer = millis();
 uint16_t id = 0;
 
 
-#define MAX_PAYLOAD_LENGTH 255
+#define MAX_PAYLOAD_LENGTH 127
 uint16_t headLen = 0;
-uint8_t payload[256];
+uint8_t payload[128];
+uint8_t payloadSend[128];
 lora2mqtt_t * m = lora2mqtt_new();
 
 uint8_t hello[20] = "{\"temperature\": 30}";
@@ -24,22 +25,24 @@ void setup() {
 void loop() {
     if (Serial.available() > 0) {
         inByte = Serial.read();
-        Serial1.write(inByte);
+        Serial.write(inByte);
     }
 
     if (Serial1.available() > 0) {
         outByte = Serial1.read();
         if (lora2mqtt_recv(payload, &headLen, outByte)) {
             if (lora2mqtt_from_binary(m, payload, headLen)) {
-                Serial.print("\nGet Type: ");
-                Serial.println(m -> type);
+                Serial.print("Get Id: ");
+                Serial.print(m -> id);
+                Serial.print(" Type: ");
+                Serial.print(m -> type);
                 if (m -> length > TYPE_LENGTH) {
-                    Serial.print("Get Data: ");
+                    Serial.print(" Data: ");
                     for (uint16_t i = 0; i < m -> length - TYPE_LENGTH; i ++) {
                         Serial.write(m -> data[i]);
                     }
-                    Serial.write('\n');
                 }
+                Serial.println("");
 
                 if (m -> type == REQUEST) {
                     lora2mqtt_set_type(m, RESPONSE);
@@ -53,7 +56,6 @@ void loop() {
             Serial.println("Error: payload to large");
             headLen = 0;
         }
-        Serial.write(outByte);
     }
 
     if (sendTimer + 10000 < millis()) {
@@ -67,16 +69,12 @@ void loop() {
 }
 
 void send_packet() {
-    Serial.println((char *)m->data);
-    lora2mqtt_to_binary(m, payload);
+    lora2mqtt_to_binary(m, payloadSend);
     uint16_t length = lora2mqtt_get_length(m);
     for (uint16_t i = 0; i < length; i ++) {
-        Serial.write(payload[i]);
-        Serial1.write(payload[i]);
+        Serial1.write(payloadSend[i]);
     }
     Serial1.write('\r');
     Serial1.write('\n');
-    Serial.write('\r');
-    Serial.write('\n');
     sendTimer = millis();
 }
